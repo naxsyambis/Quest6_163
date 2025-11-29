88803d2f419bb9d3abcc7c218e9fe7ff1195f279
naxsyambis <naxsyambis@gmail.com>
2025-11-29 16:42:27 +0700

    Setup Composable DataApp
    Menambahkan fungsi Composable DataApp sebagai wadah utama aplikasi.
    - Menginisialisasi SiswaViewModel menggunakan `viewModel()`.
    - Mengatur NavHostController menggunakan `rememberNavController()`.
    - Komponen ini akan menjadi titik masuk utama untuk navigasi dan state.

--- app/src/main/java/com/example/quest6_163/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/quest6_163/view/uicontroller/Navigasi.kt
@@ -1,6 +1,20 @@
 package com.example.quest6_163.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_163.viewmodel.SiswaViewModel
+
 enum class Navigasi {
     Formulirku,
     Detail
 }
+
+@Composable
+fun DataApp(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {}

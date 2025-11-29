29ab98ea6be9f331529f3250819aa230711ca806
naxsyambis <naxsyambis@gmail.com>
2025-11-29 16:28:58 +0700

    Setup awal Composable TampilSiswa
    Menambahkan fungsi Composable TampilSiswa.
    - Fungsi ini menerima data `Siswa` (statusSiswa) yang akan ditampilkan.
    - Mendefinisikan callback `onBackBtnClicked` untuk mendukung navigasi kembali.

---
+++ app/src/main/java/com/example/quest6_163/view/TampilanSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.quest6_163.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.quest6_163.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusSiswa: Siswa,
+    onBackBtnClicked: ()-> Unit
+) {
+
+}

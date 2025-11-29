f3ecc268a7a66a3cba5e2f8e04fb2505c1f9967c
naxsyambis <naxsyambis@gmail.com>
2025-11-29 17:28:36 +0700

    Konfigurasi MainActivity
    Menyiapkan MainActivity untuk menjalankan aplikasi Jetpack Compose.
    - Menggunakan `enableEdgeToEdge()` untuk tampilan layar penuh.
    - Memuat `DataApp` sebagai Composable utama di dalam tema aplikasi (`Quest6_163Theme`).
    - Menghapus template `Greeting` yang tidak terpakai.

--- app/src/main/java/com/example/quest6_163/MainActivity.kt
+++ app/src/main/java/com/example/quest6_163/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quest6_163.ui.theme.Quest6_163Theme
+import com.example.quest6_163.view.uicontroller.DataApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +22,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Quest6_163Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+
+                ){
+                    DataApp()
                 }
             }
         }

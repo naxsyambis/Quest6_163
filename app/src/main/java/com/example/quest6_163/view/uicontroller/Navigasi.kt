15f506f220b6e2128681359ebed7724717115364
naxsyambis <naxsyambis@gmail.com>
2025-11-29 17:08:51 +0700

    Implementasi NavHost dan rute Formulirku
    Menyiapkan NavHost di DataApp untuk mengelola transisi layar.
    - Mengatur rute awal ke Navigasi.Formulirku.
    - Menghubungkan FormSiswa ke ViewModel untuk memperbarui state data Siswa (`viewModel.setSiswa`).
    - Memicu navigasi ke rute Detail setelah data disubmit.
    - Mengubah resource ID Jenis Kelamin menjadi String untuk Composable FormSiswa.

--- app/src/main/java/com/example/quest6_163/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/quest6_163/view/uicontroller/Navigasi.kt
@@ -1,10 +1,20 @@
 package com.example.quest6_163.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.quest6_163.model.DataJK.JenisK
+import com.example.quest6_163.view.FormSiswa
+import com.example.quest6_163.view.TampilSiswa
 import com.example.quest6_163.viewmodel.SiswaViewModel
 
 enum class Navigasi {
@@ -17,4 +27,26 @@ fun DataApp(
     modifier: Modifier = Modifier,
     viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
-) {}
+) {
+    Scaffold { isiRuang->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+
+        }
+    }
+}
